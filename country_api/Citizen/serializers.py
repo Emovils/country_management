129ed76bdab4ba30e2587bfd949fc9acbf1314c4
Emovils <oviliekenelucky@gmail.com>
2025-11-01 04:59:57 -0700

    Initial commit with React, Tailwind, and API integration

--- country_api/Citizen/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Citizen  # import your model
-
-class CitizenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Citizen
-        fields = '__all__'  # or list fields like ['first_name', 'last_name', 'age']
